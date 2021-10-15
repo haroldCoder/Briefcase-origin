996f0f1cbaa4edd11d93a51236af81e68ef45834
koderAlvarex <haroldc2005@gmail.com>
2021-10-15 18:08:33 -0500

    koder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
+import $ from 'jquery';
 
 ReactDOM.render(
-    <h2>hello, koder</h2>,
+  <App/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+$("body").css("margin","0 0");
+alert(document.cookie.toString());

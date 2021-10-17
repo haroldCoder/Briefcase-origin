245f33159b6074f45a0e24b9aebc6717edfe8f6b
koderAlvarex <haroldc2005@gmail.com>
2021-10-17 16:20:15 -0500

    koder3

--- src/components/forms.js
+++ src/components/forms.js
@@ -128,8 +128,6 @@ class Forms extends Component{
 			<div className="form container">
 			<h1>{this.state.intro}</h1>
 			<form onSubmit={this.submitData}>
-				<h2>Name</h2>
-				<input type="text" className="inp" id="name" value={this.state.name} onChange={this.onChangeName}/>
 				<h2>E-mail</h2>
 				<input type="text" className="inp" id="email" value={this.state.email} onChange={this.onChangeEmail}/>
 				<h2>Password</h2>

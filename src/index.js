2e1d15d3be9feb5adbfcc3bfd33bf57c58b57fd6
koderAlvarex <haroldc2005@gmail.com>
2021-10-17 15:56:22 -0500

    koder

--- src/index.js
+++ src/index.js
@@ -4,27 +4,11 @@ import App from './App.js';
 import $ from 'jquery';
 import { initializeApp } from 'firebase/app';
 import axios from 'axios';
-import {FirebaseAppProvider} from 'reactfire';
 import Cookies from 'universal-cookie';
 import PanelP from './components/panelprofile.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCwgxApnU98xzBqeDMHW1DyfQoZs5fixJI",
-  authDomain: "brifcase-f78fd.firebaseapp.com",
-  databaseURL: "https://brifcase-f78fd-default-rtdb.firebaseio.com",
-  projectId: "brifcase-f78fd",
-  storageBucket: "brifcase-f78fd.appspot.com",
-  messagingSenderId: "628705856142",
-  appId: "1:628705856142:web:fed444af6b0557375d17df"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 ReactDOM.render(
-  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-  <App/>
-  </FirebaseAppProvider>,
+  <App/>,
   document.getElementById('root')
 );
 $("body").css("margin","0 0");
